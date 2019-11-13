c3c9dec5f88ba9f012274f7d9c88296fc6247c0f
Ben <benclarkphoto@mac.com>
2019-11-12 23:07:57 -0800

    creating HTTP request

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 second timer!')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/204809db6d811657cb813a0952ee0671/37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('0, second timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
