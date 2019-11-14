11f35a6e82554bfd6ded5e837c1f656b5650b718
Ben <benclarkphoto@mac.com>
2019-11-13 16:31:26 -0800

    customizing HTTP requests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/204809db6d811657cb813a0952ee0671/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.currently)
+    console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out.  There is a " + response.body.currently.precipProbability + "% chance of rain.")
 })
