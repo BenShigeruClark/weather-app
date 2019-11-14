0eceef7708001d01974961b4aaf2cdce62b7b71b
Ben <benclarkphoto@mac.com>
2019-11-13 23:26:17 -0800

    Handling errors

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/204809db6d811657cb813a0952ee0671/37.8267,-122.4233'
+// const url = 'https://api.darksky.net/forecast/204809db6d811657cb813a0952ee0671/37.8267,-122.4233'
 
-request({ url: url, json: true }, (error, response) => {
-    // console.log(response.body.currently)
-    console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out.  There is a " + response.body.currently.precipProbability + "% chance of rain.")
-})
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location!')
+//     } else {
+//         console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out.  There is a " + response.body.currently.precipProbability + "% chance of rain.")
+//     }
+// })
+
+
 
 const goecodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmVuY2xhcmsxIiwiYSI6ImNrMnkwY2dkdTAzMXozaHJxY3Y5aDE5ZjkifQ.6KYu8f82OMAi6dRoz23nSA'
 
 request({ url: goecodeURL, json: true}, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to api!')
+  } else if (response.body.features.length === 0) {
+    console.log('Unable to find coordinates!')
+  } else {
     const latitude = response.body.features[3].center[1]
     const longitude = response.body.features[3].center[0]
     console.log("The latitude for LA is: " + latitude)
     console.log("The longitude for LA is: " + longitude)
+  }
 })
