d4c3ac5ac1fc589f6ba1e53f405f4e97bb3f11f0
Ben <benclarkphoto@mac.com>
2019-11-19 23:40:23 -0800

    Callback Abstraction challenge

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
-const request = require('request')
+
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'https://api.darksky.net/forecast/204809db6d811657cb813a0952ee0671/37.8267,-122.4233'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location!')
-//     } else {
-//         console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out.  There is a " + response.body.currently.precipProbability + "% chance of rain.")
-//     }
-// })
-
-
-
-// const goecodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmVuY2xhcmsxIiwiYSI6ImNrMnkwY2dkdTAzMXozaHJxY3Y5aDE5ZjkifQ.6KYu8f82OMAi6dRoz23nSA'
-
-// request({ url: goecodeURL, json: true}, (error, response) => {
-//   if (error) {
-//     console.log('Unable to connect to api!')
-//   } else if (response.body.features.length === 0) {
-//     console.log('Unable to find coordinates!')
-//   } else {
-//     const latitude = response.body.features[3].center[1]
-//     const longitude = response.body.features[3].center[0]
-//     console.log("The latitude for LA is: " + latitude)
-//     console.log("The longitude for LA is: " + longitude)
-//   }
-// })
 
 
 geocode('New York', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 }) 
+
+
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
+})
