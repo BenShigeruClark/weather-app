a3841239bb109b2b34a218ec545c7109be881803
Ben <benclarkphoto@mac.com>
2019-11-20 22:12:16 -0800

    Adding destructuring and property shorthand

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ if (!address) {
   console.log('Please provide address')
   } else {
 // Use geocode callback function from goecode.js
-    geocode(address, (error, data) => {
+    geocode(address, (error, { latitude, longitude, location }) => {
       if (error) {
         return console.log(error)
       } 
 
     // Use forecast callback function from forecast.js
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return console.log(error)
       }
 
-      console.log(data.location)
+      console.log(location)
       console.log(forecastData)
     })
   }) 
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,21 +1,23 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYmVuY2xhcmsxIiwiYSI6ImNrMnkwY2dkdTAzMXozaHJxY3Y5aDE5ZjkifQ.6KYu8f82OMAi6dRoz23nSA'
-
-  request({ url: url, json: true }, (error, response) => {
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYmVuY2xhcmsxIiwiYSI6ImNrMnkwY2dkdTAzMXozaHJxY3Y5aDE5ZjkifQ.6KYu8f82OMAi6dRoz23nSA'
+                                // destructured property shorthand for body
+  request({ url, json: true }, (error, { body }) => {
       if (error) {
         callback('Unable to connect to location services!', undefined)
-      } else if (response.body.features.length === 0) {
+      } else if (body.features.length === 0) {
           callback('Unable to find location. Try another search!', undefined)
       } else {
           callback(undefined, {
-            latitude: response.body.features[0].center[1], 
-            longitude: response.body.features[0].center[0],
-            location: response.body.features[0].place_name
+            latitude: body.features[0].center[1], 
+            longitude: body.features[0].center[0],
+            location: body.features[0].place_name
           })
       }
   })
 }
 
 module.exports = geocode
+
+// encodeURIComponent(address)
